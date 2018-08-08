388f3ea72e5ea14b8d8292afce7d695c7be36810
Youssef El Jaoujat <eljoujat@gmail.com>
2018-08-08 20:46:46 +0200

    Add mavevn dependency , basic servlet and graphQl schema

---
+++ src/main/java/GraphQLEndpoint.java
@@ -0,0 +1,15 @@
+import com.coxautodev.graphql.tools.SchemaParser;
+import graphql.servlet.SimpleGraphQLServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    public GraphQLEndpoint() {
+        super(SchemaParser.newParser()
+                .file("schema.graphqls") //parse the schema file created earlier
+                .build()
+                .makeExecutableSchema());
+    }
+}
